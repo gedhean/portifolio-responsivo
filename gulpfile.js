dbca2a12c275d388135264b6f1d0eadc43445a8c
jedi <gedhean1@hotmail.com>
2017-08-30 22:55:12 -0300

    config do gulp e sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
-var gulp = require('gulp');
-var sass = require('gulp-ruby-sass');
+var gulp = require ('gulp');
+var sass = require ('gulp-ruby-sass');
+var watch = require('gulp-watch');
+
 //sass task
-gulp.task('sass', function(){
-	return sass('sass/*.sass').pipe(gulp.dest('css'));
+gulp.task('sass', function () {
+	return sass('sass/**/*.sass').pipe(gulp.dest('css'));
+});
+
+//task watch
+gulp.task('watch', function () {
+	gulp.watch('sass/**/*.sass', ['sass']);
 });
 
 //defualt task
-gulp.task('default', function(){
-	console.log("olá");
-});
+gulp.task('default', ['sass', 'watch']);
